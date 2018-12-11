c518d289443ff5fee09afa50a299cb596dd30f73
Cairbr <cairbr@users.noreply.github.com>
2018-12-11 19:07:39 +0100

    Adding middlewares

--- node.js
+++ node.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
 
 var app = express();
+
+app.use(morgan('combined')) //Active le middleware de logging
+.use(express.static(__dirname + '/public')) //indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+.use(favicon(__dirname + '/public/favicon.ico')) //active la favicon indiquée
+/*.use(function(req, res){
+    res.send('Hello bruh');
+  
+})  */
+;
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
